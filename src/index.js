15f50cadb1575068627e426948dcd23086dd95e4
Intelli-Dev <intellidevs5@gmail.com>
2022-02-25 18:19:00 +0100

    database connection

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBEoLUt8FXMzhYLsbWjtPwOz3EhvGBkUQM",
+    authDomain: "fir-9-c3738.firebaseapp.com",
+    projectId: "fir-9-c3738",
+    storageBucket: "fir-9-c3738.appspot.com",
+    messagingSenderId: "996076587116",
+    appId: "1:996076587116:web:60e295570537c14f4e3049"
+  };
+  initializeApp(firebaseConfig)
